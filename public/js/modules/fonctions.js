49eff55e119402286581f008e21bb373bf3f7494
Melonours <adrianrumi@hotmail.com>
2025-01-15 12:18:34 +0100

    html css finis

---
+++ public/js/modules/fonctions.js
@@ -0,0 +1,27 @@
+import { generateBtn, myBody,  } from "./variables.js";
+
+function hello(){
+    console.log("Bonjour kaurantun");
+}
+// ! Fonction pour copier la valeur de l'input dans le clipboard
+// ! Vérifier les checkbox et selon si checked, lancer la génération du tableau avec les caractères
+// ! Fonction generate qui va lancer les verif des fonctions checked
+// ! Récupérer la valeur de l'input  number 
+// ! Evenement sur le bouton qui va lancer la fonction generate
+
+// ! fonction sur chaque tableau de caractères qui va randomiser la valeur sortie
+
+
+function generate(){
+    // console.log("salut depuis la fonction generate");
+
+}
+generateBtn.addEventListener("click", () => generate())
+
+function copyPassword(){
+    
+}
+
+
+
+export { hello }
---
+++ public/js/modules/variables.js
@@ -0,0 +1,11 @@
+let myBody = document.body
+console.log(myBody);
+
+let generateBtn = document.getElementById("generate")
+console.log(generateBtn);
+
+let copyBtn = document.getElementById("copy")
+// console.log(copyBtn);
+
+
+export { myBody,generateBtn, copyBtn }
